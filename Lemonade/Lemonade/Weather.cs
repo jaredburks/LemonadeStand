48238d27f4022e2b5faa0ea18767561595655706
jaredburks <jarabu93@gmail.com>
2017-09-12 14:38:08 -0500

    Set up methods for weather class.

--- Lemonade/Lemonade/Weather.cs
+++ Lemonade/Lemonade/Weather.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//This class will have both weather and forecast.
+//The difference between the two will be a slight change in Temp. and weather type(sunny, cloudly, partly-cloudy, rainy) if any.
 namespace Lemonade
 {
     class Weather
     {
+        public void Temperature()//Randomize temp in degrees
+        {
+
+        }
+
+        public void WeatherType()//Randomize weather type
+        {
+
+        }
+
+        public void Forecast()//Display Temp and weather type
+        {
+
+        }
+
+        public void ActualWeather()//Takes Forecast and changes results by a few degrees or between one of the weatherTypesS
+        {
+
+        }
     }
 }
