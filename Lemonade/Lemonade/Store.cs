96394b02e8833c4f5455c21096b976d3fce11a41
jaredburks <jarabu93@gmail.com>
2017-09-15 14:54:44 -0500

    finished sell supply methods in store class.

--- Lemonade/Lemonade/Store.cs
+++ Lemonade/Lemonade/Store.cs
@@ -20,7 +20,7 @@ namespace Lemonade
         {
             this.player = player;
         }
-        
+
         public void SellToPlayer()
         {
             Console.WriteLine("Welcome to the store, whatchu want?");
@@ -30,10 +30,8 @@ namespace Lemonade
                     SellLemons();
                     break;
                 case "2"://buy sugar
-                    SellSugar();
                     break;
                 case "3"://buy ice
-                    SellIce();
                     break;
                 case "4"://buy cups
                     SellCups();
@@ -60,7 +58,7 @@ namespace Lemonade
             {
                 case "1":
                     player.wallet -= player.wallet - .60;
-                    for(int i = 0; i < 10; i++)
+                    for (int i = 0; i < 10; i++)
                     {
                         stash.BuyLemons();
                     }
@@ -96,19 +94,140 @@ namespace Lemonade
             //25 for 1
             //50 for 1.75
             //100 for 3.25
-
+            string deal = Console.ReadLine();
+            Console.WriteLine("Enter 1 for 25 for 1.00");
+            Console.WriteLine("Enter 2 for 50 for 1.75");
+            Console.WriteLine("Enter 3 for 100 for 3.25");
+            Console.WriteLine("Any other option will bring you back to store");
+            Console.WriteLine("How many cups would you like to buy?");
+            switch (deal)
+            {
+                case "1":
+                    player.wallet -= player.wallet - 1.00;
+                    for (int i = 0; i < 25; i++)
+                    {
+                        stash.BuyCups();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " cups");
+                    SellToPlayer();
+                    break;
+                case "2":
+                    player.wallet -= player.wallet - 1.75;
+                    for (int i = 0; i < 50; i++)
+                    {
+                        stash.BuyCups();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " cups");
+                    SellToPlayer();
+                    break;
+                case "3":
+                    player.wallet -= player.wallet - 3.25;
+                    for (int i = 0; i < 100; i++)
+                    {
+                        stash.BuyCups();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " cups");
+                    SellToPlayer();
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again or continue to Recipe");
+                    SellToPlayer();
+                    break;
+            }
         }
+
         public void SellIce()
         {
-            //100 for 90
+            //100 for .90
             //250 for 2.70
-            //500 for 4
+            //500 for 4.00
+            string deal = Console.ReadLine();
+            Console.WriteLine("Enter 1 for 100 for .90");
+            Console.WriteLine("Enter 2 for 250 for 2.70");
+            Console.WriteLine("Enter 3 for 500 for 4.00");
+            Console.WriteLine("Any other option will bring you back to store");
+            Console.WriteLine("How many icecubes would you like to buy?");
+            switch (deal)
+            {
+                case "1":
+                    player.wallet -= player.wallet - .90;
+                    for (int i = 0; i < 100; i++)
+                    {
+                        stash.BuyIce();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " icecubes");
+                    SellToPlayer();
+                    break;
+                case "2":
+                    player.wallet -= player.wallet - 2.70;
+                    for (int i = 0; i < 250; i++)
+                    {
+                        stash.BuyIce();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " icecubes");
+                    SellToPlayer();
+                    break;
+                case "3":
+                    player.wallet -= player.wallet - 4.00;
+                    for (int i = 0; i < 500; i++)
+                    {
+                        stash.BuyIce();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " icecubes");
+                    SellToPlayer();
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again or continue to Recipe");
+                    SellToPlayer();
+                    break;
+            }
         }
+
         public void SellSugar()
         {
             //10 for .70
             //25 for 1.40
             //50 for 2.60
+            string deal = Console.ReadLine();
+            Console.WriteLine("Enter 1 for 10 for .70");
+            Console.WriteLine("Enter 2 for 30 for 1.40");
+            Console.WriteLine("Enter 3 for 75 for 2.60");
+            Console.WriteLine("Any other option will bring you back to store");
+            Console.WriteLine("How much cups of sugar would you like to buy?");
+            switch (deal)
+            {
+                case "1":
+                    player.wallet -= player.wallet - .70;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        stash.BuySugar();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " cups of sugar");
+                    SellToPlayer();
+                    break;
+                case "2":
+                    player.wallet -= player.wallet - 1.40;
+                    for (int i = 0; i < 25; i++)
+                    {
+                        stash.BuySugar();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " cups of sugar");
+                    SellToPlayer();
+                    break;
+                case "3":
+                    player.wallet -= player.wallet - 2.60;
+                    for (int i = 0; i < 50; i++)
+                    {
+                        stash.BuySugar();
+                    }
+                    Console.WriteLine(player.name + " has " + stash.lemons.Count + " cups of sugar");
+                    SellToPlayer();
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again or continue to Recipe");
+                    SellToPlayer();
+                    break;
+            }
         }
     }
 }
