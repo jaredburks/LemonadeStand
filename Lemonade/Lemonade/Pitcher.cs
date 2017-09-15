5b0097561305aeaa7e124ebe59ef4699d4485ef2
jaredburks <jarabu93@gmail.com>
2017-09-15 15:37:51 -0500

    Added new classes.

---
+++ Lemonade/Lemonade/Pitcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Will take in amount of supplies from recipe. 
+namespace Lemonade
+{
+    class Pitcher
+    {
+    }
+}
---
+++ Lemonade/Lemonade/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Will show the amount of supplies to subtract from inventory and be put in pitcher.
+namespace Lemonade
+{
+    class Recipe
+    {
+    }
+}
